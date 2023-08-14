f0f7353c2005e119f5b1036a043763403d77bde7
Imotytskiy <Imotytskiy@gmail.com>
2023-08-14 12:40:46 +0200

    add

--- lessons/hw33v/src/routes/photos.jsx
+++ lessons/hw33v/src/routes/photos.jsx
@@ -1,14 +1,51 @@
-import { NavLink, useNavigation } from "react-router-dom";
+import { NavLink, useNavigation, useParams } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 
 export default function Photos() {
   const { photos } = useLoaderData();
+  const { albumId } = useParams();
   const { state } = useNavigation();
+
+  const currentAlbum = photos.find((album) => album.id === parseInt(albumId));
+
+  if (state === "loading") {
+    return <p> LOADING </p>;
+  }
+
   return (
-    <>
-      {state === "loading" ? (
-        <p> LOADING </p>
-      ) : (
+    <div>
+      <table>
+        <thead>
+          {currentAlbum && (
+            <tr>
+              <th scope="col">Album Title</th>
+              <th scope="col">Album Description (if any)</th>
+            </tr>
+          )}
+        </thead>
+        <tbody>
+          {currentAlbum && (
+            <tr>
+              <td>{currentAlbum.title}</td>
+              <td>{currentAlbum.id}</td>
+              <td>
+                <NavLink className="button-denied" to="/">
+                  ПОВЕРНУТИСЯ
+                </NavLink>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Thumbnail</th>
+            <th scope="col">Photo Title</th>
+            <th scope="col">Photo ID</th>
+          </tr>
+        </thead>
         <tbody>
           {photos.map((photo) => (
             <tr key={photo.id}>
@@ -17,15 +54,13 @@ export default function Photos() {
               </td>
               <td>{photo.title}</td>
               <td>{photo.id}</td>
-              <td>
-                <NavLink  className="button-album" to="/">
-                  ПОВЕРНУТИСЯ
-                </NavLink>
-              </td>
             </tr>
           ))}
         </tbody>
-      )}
-    </>
+      </table>
+      <NavLink className="button-denied" to="/">
+        ПОВЕРНУТИСЯ
+      </NavLink>
+    </div>
   );
 }
