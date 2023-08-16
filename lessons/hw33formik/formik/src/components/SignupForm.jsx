6ca5ad57217ddd9a98463d7a571f02c07570ccb7
Imotytskiy <Imotytskiy@gmail.com>
2023-08-16 23:27:24 +0200

    add validation

--- lessons/hw33formik/formik/src/components/SignupForm.jsx
+++ lessons/hw33formik/formik/src/components/SignupForm.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Formik } from "formik";
+import "../index";
 
 const FormSignup = () => (
   <div>
     <h1>HW 33. formik</h1>
     <Formik
-      initialValues={{ firstname: "", email: "", password: "" }}
+      initialValues={{ firstname: "", email: "", tel: "" }}
       validate={(values) => {
         const errors = {};
+
+        if (!values.firstname) {
+          errors.firstname = "Required";
+        } else if (
+          /[\d]/.test(values.firstname) ||
+          values.firstname.length < 2 ||
+          values.firstname.length > 20
+        ) {
+          errors.firstname = "Invalid name";
+        }
+
         if (!values.email) {
           errors.email = "Required";
         } else if (
@@ -15,6 +27,16 @@ const FormSignup = () => (
         ) {
           errors.email = "Invalid email address";
         }
+
+        if (!values.tel) {
+          errors.tel = "Required";
+        } else if (
+          !/^[0-9]+$/i.test(String(values.tel)) ||
+          String(values.tel).length !== 12
+        ) {
+          errors.tel = "Тільки цифри та довжина 12";
+        }
+
         return errors;
       }}
       onSubmit={(values, { setSubmitting }) => {
@@ -32,39 +54,44 @@ const FormSignup = () => (
         handleBlur,
         handleSubmit,
         isSubmitting,
-        /* and other goodies */
       }) => (
         <form onSubmit={handleSubmit}>
-          <label htmlFor="firstname">Ім'я</label>
+          <label htmlFor="name">Ім'я</label>
           <input
-            id="firstname"
+            id="name"
+            type="text"
+            name="firstname"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.name}
+          />
+          {errors.name && touched.name && (
+            <div className="error">{errors.name}</div>
+          )}
+          <label htmlFor="email">E-mail</label>
+          <input
+            id="email"
             type="email"
             name="email"
             onChange={handleChange}
             onBlur={handleBlur}
             value={values.email}
           />
-          {errors.email && touched.email && errors.email}
-          <label htmlFor="email">EMAIL</label>
-          <input
-            id="email"
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && touched.password && errors.password}
+          {errors.email && touched.email && (
+            <div className="error">{errors.email}</div>
+          )}
           <label htmlFor="tel">телефон</label>
           <input
             id="tel"
-            type="password"
-            name="password"
+            type="text"
+            name="tel"
             onChange={handleChange}
             onBlur={handleBlur}
-            value={values.password}
+            value={values.tel}
           />
-          {errors.password && touched.password && errors.password}
+          {errors.tel && touched.tel && (
+            <div className="error">{errors.tel}</div>
+          )}
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
