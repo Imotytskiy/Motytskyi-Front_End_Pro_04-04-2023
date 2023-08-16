d28e49e1cd1266c58a74c05fc6ed5e0c2a981387
Imotytskiy <Imotytskiy@gmail.com>
2023-08-16 22:26:59 +0200

    add labels

--- lessons/hw33formik/formik/src/components/SignupForm.jsx
+++ lessons/hw33formik/formik/src/components/SignupForm.jsx
@@ -1,27 +1,77 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 
-const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
+const FormSignup = () => (
+  <div>
+    <h1>HW 33. formik</h1>
+    <Formik
+      initialValues={{ firstname: "", email: "", password: "" }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="firstname">Ім'я</label>
+          <input
+            id="firstname"
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <label htmlFor="email">Ім'я</label>
+          <input
+            id="email"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <label htmlFor="tel">Ім'я</label>
+          <input
+            id="tel"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
 
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+export default FormSignup;
