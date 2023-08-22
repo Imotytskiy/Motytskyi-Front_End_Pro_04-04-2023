f90f945f0179f1901135cc45f4f9878965e0aeab
Imotytskiy <Imotytskiy@gmail.com>
2023-08-22 13:19:48 +0300

    add

---
+++ lessons/hw36/src/components/Button/Button.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = ({ buttonClick, inputValue, setInputValue }) => {
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div className={styles.div}>
+      <input
+        className={styles.input}
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <button className={styles.button} onClick={buttonClick}>
+        СПРАВА
+      </button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ lessons/hw36/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import TodoItem from "../TodoItem/TodoItem";
+import styles from "./TodoList.module.css";
+
+export default function TodoList({ todos, buttonDelClick }) {
+  return (
+    <ul className={styles.ul}>
+      {todos.map((todo, index) => (
+        <TodoItem
+          todo={todo}
+          index={index}
+          key={todo.id}
+          buttonDelClick={buttonDelClick}
+        />
+      ))}
+    </ul>
+  );
+}
