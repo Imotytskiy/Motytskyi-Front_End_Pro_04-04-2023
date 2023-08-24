046cfcd206500b9e95485b102de2b12e2a835815
Imotytskiy <Imotytskiy@gmail.com>
2023-08-24 15:13:59 +0300

    add update

--- lessons/hw36mui/src/components/TodoItem/TodoItem.jsx
+++ lessons/hw36mui/src/components/TodoItem/TodoItem.jsx
@@ -4,37 +4,41 @@ import { useDispatch } from "react-redux";
 import { toggleTodo } from "../../store/store";
 import Checkbox from '@mui/material/Checkbox';
 import { ListItem, ListItemIcon, ListItemButton, IconButton } from "@mui/material";
+import styles from './TodoItem.module.css'
 
 export default function TodoItem({ todo, index, buttonDelClick }) {
   const dispatch = useDispatch();
+
   const handleClick = () => {
     dispatch(toggleTodo(todo.id));
   };
 
   return (
-    <ListItem onClick={handleClick}
-    secondaryAction={
-      <IconButton edge="end" aria-label="comments">
-        <DelButton todo={todo} buttonDelClick={buttonDelClick} />
-      </IconButton>}
-     disablePadding>
-      <ListItemButton  role={undefined} dense>
+    <ListItem
+      onClick={handleClick}
+      secondaryAction={
+        <IconButton edge="end" aria-label="comments">
+          <DelButton todo={todo} buttonDelClick={buttonDelClick} />
+        </IconButton>
+      }
+      disablePadding
+    >
+      <ListItemButton role={undefined} dense>
         <ListItemIcon>
           <Checkbox
             checked={todo.completed}
             onChange={handleClick}
             color="secondary"
             inputProps={{ 'aria-label': 'controlled' }}
-            readOnly
           />
         </ListItemIcon>
         <span
-  style={
-    todo.completed ? { textDecoration: 'line-through', color: 'red' } : { textDecoration: 'none' }
-  }
->
-  <b>{index + 1}.&ensp; </b>{todo.title}
-</span>
+          className={
+            todo.completed ? styles.inputTextCrossed : styles.inputText
+          }
+        >
+          <b>{index + 1}.&ensp; </b>{todo.title}
+        </span>
       </ListItemButton>
     </ListItem>
   );
