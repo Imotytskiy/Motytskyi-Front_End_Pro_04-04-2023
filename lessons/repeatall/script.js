8bf78a7432480d1648faaf4ed9b4f1b8c4133b5d
Imotytskiy <Imotytskiy@gmail.com>
2023-08-27 17:45:51 +0300

    add

--- lessons/repeatall/script.js
+++ lessons/repeatall/script.js
@@ -1,4 +1,6 @@
-"use strict";
+// "use strict";
+
+
 // First level
 
 // let sum = 0;
@@ -10,16 +12,45 @@
 
 // Second level
 
-const numRegex = /^-?\d*\.?\d+(e[-+]?\d+)?$/;
-let num,
-  sum = 0;
-for (let i = 1; i <= 3; ++i) {
-  num = Number(prompt(`Введіть число ${i}`, "").replace(/,/g, "."));
-  if (numRegex.test(num)) {
-    sum += num;
-  } else {
-    alert("Тільки числа:");
-    i--;
-  }
-}
-alert(`Середнє арифметичне ${(sum / 3).toFixed(2)}`);
+// const numRegex = /^-?\d*\.?\d+(e[-+]?\d+)?$/;
+// let num,
+//   sum = 0;
+// for (let i = 1; i <= 3; ++i) {
+//   num = Number(prompt(`Введіть число ${i}`, "").replace(/,/g, "."));
+//   if (numRegex.test(num)) {
+//     sum += num;
+//   } else {
+//     alert("Тільки числа:");
+//     i--;
+//   }
+// }
+// alert(`Середнє арифметичне ${(sum / 3).toFixed(2)}`);
+
+
+// const width = 5;
+// const height = 10;
+
+// function calcRectArea(width, height) {
+//   return width*height;
+// }
+
+// console.log(calcRectArea(width,height));
+
+// class Rectangle {
+  // width;
+  // height;
+ 
+// constructor(w , h) {
+//    this.width = w;
+//    this.height = h;
+// }
+
+//   calcArea(){
+//     return this.width * this.height;
+//   }
+// }
+// const rect = new Rectangle ( w = 5, h = 10)
+// console.log(rect.calcArea());
+
+let num = 42;
+  
