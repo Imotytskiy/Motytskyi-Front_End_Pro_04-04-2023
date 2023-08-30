3860fabfed9cfc887b8bc52479144af373cadb60
Imotytskiy <Imotytskiy@gmail.com>
2023-08-30 23:00:17 +0300

    add

---
+++ lessons/repeatall/array.js
@@ -0,0 +1,13 @@
+const arr = [1,2,3]
+const arr1 = [1,2,3]
+
+// if (arr === arr1) {
+//     console.log('True')
+// } 
+// console.log('False')
+let ewq = 2434;
+arr.push(ewq);
+console.log('123')
+console.log(arr);
+
+const finded = people.find(p) => p.bu
